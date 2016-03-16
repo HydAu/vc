3a42e634213de312320e2295d024ac71cd8a04f3
artem-dudarev <dartal2@gmail.com>
2016-03-17 00:29:32 +0200

    Started to implement Elasticsearch provider based on Nest client.

---
+++ PLATFORM/Modules/Search/VirtoCommerce.SearchModule.Data/Providers/ElasticsearchNest/NestDocument.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace VirtoCommerce.SearchModule.Data.Providers.ElasticsearchNest
+{
+    public class NestDocument : Dictionary<string, object>
+    {
+        public object Id
+        {
+            get
+            {
+                if (ContainsKey("__key"))
+                    return this["__key"];
+
+                return null;
+            }
+            set
+            {
+                if (ContainsKey("__key"))
+                    this["__key"] = value;
+                else
+                    Add("__key", value);
+            }
+        }
+    }
+}
---
+++ PLATFORM/Modules/Search/VirtoCommerce.SearchModule.Data/Providers/ElasticsearchNest/NestProvider.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Elasticsearch.Net;
+using Nest;
+using VirtoCommerce.Domain.Search.Model;
+using VirtoCommerce.Domain.Search.Services;
+using IDocument = VirtoCommerce.Domain.Search.Model.IDocument;
+
+namespace VirtoCommerce.SearchModule.Data.Providers.ElasticsearchNest
+{
+    public class NestProvider : ISearchProvider
+    {
+        public NestProvider(ISearchQueryBuilder queryBuilder)
+        {
+            QueryBuilder = queryBuilder;
+        }
+
+        public ISearchQueryBuilder QueryBuilder { get; private set; }
+
+        public void Close(string scope, string documentType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Commit(string scope)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Index(string scope, string documentType, IDocument document)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Remove(string scope, string documentType, string key, string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAll(string scope, string documentType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISearchResults Search(string scope, ISearchCriteria criteria)
+        {
+            var client = new ElasticClient();
+            var query = QueryBuilder.BuildQuery(criteria);
+            var response = client.Search<NestDocument>(s => s.Index(scope).Type(criteria.DocumentType).QueryRaw(query.ToString()));
+
+            // Parse documents returned
+            var docList = new List<IDocument>();
+            foreach (var indexDoc in response.Documents)
+            {
+                var document = new ResultDocument();
+                foreach (var field in indexDoc.Keys)
+                    document.Add(new DocumentField(field, indexDoc[field]));
+                docList.Add(document);
+            }
+
+            var documents = new ResultDocumentSet
+            {
+                TotalCount = (int)response.Total,
+                Documents = docList.ToArray()
+            };
+
+            // Create search results object
+            var results = new SearchResults(criteria, new[] { documents })
+            {
+                FacetGroups = CreateFacets(criteria, response)
+            };
+
+            return results;
+        }
+
+
+        private static FacetGroup[] CreateFacets(ISearchCriteria criteria, ISearchResponse<NestDocument> response)
+        {
+            return null;
+        }
+    }
+}
